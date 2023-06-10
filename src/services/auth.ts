fdf4e16b3a41dd1b4e351aa74d413fdd6603e8e3
VictoriaaRoman <victoria.roman@estudiantat.upc.edu>
2023-06-10 20:40:04 +0200

    Google Login

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -27,6 +27,26 @@ const registerNewUser=async(user:User)=>{
     const newUser = await insertUser(user);
     return newUser;
 };
+const registerNewUserGoogle=async(user:User)=>{
+    // Validate the email format
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const isValidEmail = emailRegex.test(user.email);
+    if (!isValidEmail) {
+        return "INVALID_EMAIL";
+    }
+    // Check if the password is blank
+    if (user.password.trim() === "") {
+        return "BLANK_PASSWORD";
+    }
+    // Check if the user already exists
+    const existingUser = await UserModel.findOne({ email: user.email });
+    if (!existingUser) {
+        // Encrypt the user's password and insert the user into the database
+        user.password = await encrypt(user.password);
+        const newUser = await insertUser(user);
+        return newUser;
+    }
+};
 
 const loginUser=async( email:string, password: string)=>{
     const checkIs =await UserModel.findOne({email})
@@ -47,4 +67,4 @@ const loginUser=async( email:string, password: string)=>{
     return data;
 };
 
-export{registerNewUser,loginUser};
+export{registerNewUser,loginUser, registerNewUserGoogle};
